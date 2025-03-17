8fe1d6b2785222c018ccf36462113699ec856d8f
Auraticus <auraticus@gmail.com>
2025-03-17 20:23:24 +0200

    Navbar added

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export default function NavbarComponent(){
+    return (
+        <>
+            <p className={styles.navbarTest}>Navbar</p>
+        </>
+    )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import NavbarComponent from "../Components/Navbar";
+
+export default function HomePage(){
+    return (
+        <>
+            <NavbarComponent />
+        </>
+    )
+}
